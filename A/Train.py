c388c24376f6aa0b0f8d76a9113844bf2dccd3b1
MohammedZweiri <mohammedzweiri@hotmail.com>
2024-12-08 19:52:22 +0000

    feat: sub-folders renamed to be A & B. Train.py (from A and B) updated to have the decision tree and CNN models respectively

--- A/Train.py
+++ A/Train.py
@@ -6,33 +6,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #==========this libraries for the models =============
-from tensorflow import keras
-from keras.utils import to_categorical, plot_model
-import tensorflow as tf
-# from tensorflow.keras import optimizers
-#from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from sklearn.ensemble import RandomForestClassifier
 from PIL import ImageFont
 
  
 #========== This libraries for getting the result of accurcy and confusion matrix of the model =======
 from sklearn.metrics import confusion_matrix, classification_report,accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, ConfusionMatrixDisplay
 from pathlib import Path
-from keras.models import model_from_json
-from sklearn.utils import class_weight
-import visualkeras
+from sklearn import tree
 import medmnist
 from medmnist import INFO, Evaluator
 
-class_labels = ['basophil',
-                'eosinophil',
-                'erythroblast',
-                'immature granulocytes',
-                'lymphocyte',
-                'monocyte',
-                'neutrophil',
-                'platelet']
+class_labels = []
 
 def dataset_download(dataset_name):
 
@@ -42,34 +27,33 @@ def dataset_download(dataset_name):
     
     dataset_info = INFO[dataset_name]
 
-
+    #print(dataset_info)
     dataset_class = getattr(medmnist, dataset_info['python_class'])
 
 
     train_dataset = dataset_class(split='train', download=download)
     validation_dataset = dataset_class(split='val', download=download)
     test_dataset = dataset_class(split='test', download=download)
-
     print("Training Dataset")
     
 
     if train_dataset.imgs.ndim == 3:
         print("Adding channel to images...")
-        train_dataset.imgs = np.expand_dims(train_dataset.imgs, axis=1)
+        train_dataset.imgs = np.expand_dims(train_dataset.imgs, axis=-1)
 
     print("Validation Dataset")
     
 
     if validation_dataset.imgs.ndim == 3:
         print("Adding channel to images...")
-        validation_dataset.imgs = np.expand_dims(validation_dataset.imgs, axis=1)
+        validation_dataset.imgs = np.expand_dims(validation_dataset.imgs, axis=-1)
 
     print("Testing Dataset")
     
 
     if test_dataset.imgs.ndim == 3:
         print("Adding channel to images...")
-        test_dataset.imgs = np.expand_dims(test_dataset.imgs, axis=1)
+        test_dataset.imgs = np.expand_dims(test_dataset.imgs, axis=-1)
 
     print("Shapes of images")
     print(f"Training shape: {str(train_dataset.imgs.shape)}")
@@ -79,19 +63,19 @@ def dataset_download(dataset_name):
     return train_dataset, validation_dataset, test_dataset
 
 def preprocess_check(train_dataset, validation_dataset, test_dataset):
-    if len(train_dataset.imgs) != 11959:
+    if len(train_dataset.imgs) != 546 :
         print("Missing images detected in the training dataset")
         print(f"Found {len(train_dataset.imgs)}, should be 11959.")
     else:
         print("SUCCESS: No missing images in training dataset.")
 
-    if len(validation_dataset.imgs) != 1712:
+    if len(validation_dataset.imgs) != 78:
         print("Missing images detected in the validation dataset")
         print(f"Found {len(validation_dataset.imgs)}, should be 1712.")
     else:
         print("SUCCESS: No missing images in validation dataset.")
 
-    if len(test_dataset.imgs) != 3421:
+    if len(test_dataset.imgs) != 156:
         print("Missing images detected in the test dataset")
         print(f"Found {len(test_dataset.imgs)}, should be 3421.")
     else:
@@ -103,6 +87,39 @@ def normalize_dataset(train_dataset, validation_dataset, test_dataset):
     test_dataset.imgs = test_dataset.imgs/255.0
 
     return train_dataset, validation_dataset, test_dataset
+
+def visualise_subset(train_dataset):
+    fig = plt.figure(figsize=(12,10))
+    for x in range(9):
+        value = 330+1+x
+        plt.subplot(value)
+        plt.imshow(train_dataset.imgs[x], cmap=plt.get_cmap('gray'))
+        plt.title(f"Class {train_dataset.labels[x][0]}")
+    
+    plt.savefig("figures/subset_images.jpeg")
+    plt.close()
+
+def descision_tree():
+
+    accuracy_test_score = []
+    maximum_tree_depth = []
+    maximum_features = []
+
+    for maximum_features in range(1,30):
+        for maximum_depth in range(1,30):
+            classifier = tree.DecisionTreeClassifier(max_features=maximum_features, max_depth=maximum_depth, criterion='entropy', random_state=0)
+            classifier
+    tree_params={
+    'criterion':'entropy'
+        }
+    clf = tree.DecisionTreeClassifier( **tree_params )
+
+    return None
+
+def random_forrest():
+
+
+    return None
 
 def save_model(model, model_name):
     model_structure = model.to_json()
@@ -165,34 +182,13 @@ def plot_accuray_loss(model_history):
     ax2.grid()
     fig.savefig('figures/CNN_accuracy_loss_test1.png')
 
-def data_augmentation_visualisation(train_dataset):
-    datagen = tf.keras.Sequential([
-        layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
-        layers.experimental.preprocessing.RandomRotation(0.2)])
-    return datagen
 
-def class_imbalance_handling(train_dataset):
-    blood_class_weights = class_weight.compute_class_weight('balanced',
-                                                         classes = np.unique(train_dataset.labels[:,0]),
-                                                         y = train_dataset.labels[:, 0])
-
-    weights = {0 : blood_class_weights[0], 
-            1 : blood_class_weights[1], 
-            2 : blood_class_weights[2], 
-            3 : blood_class_weights[3], 
-            4 : blood_class_weights[4], 
-            5 : blood_class_weights[5], 
-            6 : blood_class_weights[6], 
-            7 : blood_class_weights[7] }
-
-    print(f"Class weights for imbalance {weights}")
-    return weights
 
 
 if __name__=="__main__":
 
 
-    dataset_name = "bloodmnist"
+    dataset_name = "breastmnist"
 
     train_dataset, validation_dataset, test_dataset = dataset_download(dataset_name)
 
@@ -200,56 +196,60 @@ if __name__=="__main__":
 
     train_dataset, validation_dataset, test_dataset = normalize_dataset(train_dataset, validation_dataset, test_dataset)
 
+    visualise_subset(train_dataset)
+    # print(validation_dataset)
+    # print(test_dataset)
+
 
     # One-hot encoding of labels (multi-class classification)
-    train_labels = to_categorical(train_dataset.labels, num_classes=8)
-    val_labels = to_categorical(validation_dataset.labels, num_classes=8)
+    # train_labels = to_categorical(train_dataset.labels, num_classes=8)
+    # val_labels = to_categorical(validation_dataset.labels, num_classes=8)
 
-    model = Sequential()
+    # model = Sequential()
 
-    model.add(Conv2D(32, (3,3), padding='same', input_shape=(28,28,3), activation="relu"))
-    model.add(Conv2D(32, (3,3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+    # model.add(Conv2D(32, (3,3), padding='same', input_shape=(28,28,3), activation="relu"))
+    # model.add(Conv2D(32, (3,3), activation="relu"))
+    # model.add(MaxPooling2D(pool_size=(2,2)))
+    # model.add(Dropout(0.25))
 
-    model.add(Conv2D(64, (3,3), padding='same', activation="relu"))
-    model.add(Conv2D(64, (3,3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+    # model.add(Conv2D(64, (3,3), padding='same', activation="relu"))
+    # model.add(Conv2D(64, (3,3), activation="relu"))
+    # model.add(MaxPooling2D(pool_size=(2,2)))
+    # model.add(Dropout(0.25))
 
-    model.add(Flatten())
-    model.add(Dense(512, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(8, activation="softmax"))
+    # model.add(Flatten())
+    # model.add(Dense(512, activation="relu"))
+    # model.add(Dropout(0.5))
+    # model.add(Dense(8, activation="softmax"))
 
-    print(model.summary())
-    plot_model(model, 
-               to_file='figures/CNN_Model_test1.png', 
-               show_shapes=True,
-                show_dtype=False,
-                show_layer_names=False,
-                rankdir="TB",
-                expand_nested=False,
-                dpi=200,
-                show_layer_activations=True,
-                show_trainable=False)
+    # print(model.summary())
+    # plot_model(model, 
+    #            to_file='figures/CNN_Model_test1.png', 
+    #            show_shapes=True,
+    #             show_dtype=False,
+    #             show_layer_names=False,
+    #             rankdir="TB",
+    #             expand_nested=False,
+    #             dpi=200,
+    #             show_layer_activations=True,
+    #             show_trainable=False)
 
-    model.compile(loss='categorical_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    # model.compile(loss='categorical_crossentropy',
+    #           optimizer='adam',
+    #           metrics=['accuracy'])
     
-    weights = class_imbalance_handling(train_dataset)
-    history = model.fit(train_dataset.imgs, train_labels, 
-              epochs=10,
-              callbacks=[tf.keras.callbacks.EarlyStopping(patience=10)],
-              validation_data=(validation_dataset.imgs, val_labels),
-            shuffle=True,
-            class_weight=weights)
+    # weights = class_imbalance_handling(train_dataset)
+    # history = model.fit(train_dataset.imgs, train_labels, 
+    #           epochs=10,
+    #           callbacks=[tf.keras.callbacks.EarlyStopping(patience=10)],
+    #           validation_data=(validation_dataset.imgs, val_labels),
+    #         shuffle=True,
+    #         class_weight=weights)
     
-    save_model(model, "CNN_model_task1")
+    # save_model(model, "CNN_model_task1")
 
-    test_dataset_prob = model.predict(test_dataset.imgs, verbose=0)
-    test_predict_labels = np.argmax(test_dataset_prob, axis=-1)
-    evaluate_model(test_dataset.labels, test_predict_labels, test_dataset_prob, class_labels)
+    # test_dataset_prob = model.predict(test_dataset.imgs, verbose=0)
+    # test_predict_labels = np.argmax(test_dataset_prob, axis=-1)
+    # evaluate_model(test_dataset.labels, test_predict_labels, test_dataset_prob, class_labels)
 
-    plot_accuray_loss(history)
+    # plot_accuray_loss(history)
