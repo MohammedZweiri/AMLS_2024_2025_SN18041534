63f541bf1360a80d2f2ab960b173a8ac04603698
MohammedZweiri <mohammedzweiri@hotmail.com>
2025-01-02 20:43:02 +0000

    feat: cNN_A.py file updated to have the final version of the CNN model

--- A/CNN_A.py
+++ A/CNN_A.py
@@ -17,23 +17,17 @@ os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 #==========this libraries for the models =============
 from tensorflow import keras
-from keras.utils import to_categorical, plot_model
+from keras.utils import plot_model
 import tensorflow as tf
 import torch
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense, BatchNormalization, RandomFlip, RandomZoom, RandomRotation
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from keras.optimizers import Adam
-from keras.regularizers import l2, l1
  
 #========== This libraries for getting the result of accurcy and confusion matrix of the model =======
 from sklearn.metrics import confusion_matrix, classification_report,accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, ConfusionMatrixDisplay
-from pathlib import Path
-from sklearn.utils import shuffle
-from keras.models import model_from_json
 from keras import Input
 from sklearn.utils import class_weight
-import visualkeras
-from medmnist import INFO
 from src import utils
 
 
@@ -69,7 +63,8 @@ def preprocess_check(train_dataset, validation_dataset, test_dataset):
         print("SUCCESS: No missing images in test dataset.")
 
 
-def evaluate_model(true_labels, predict_probs, label_names):
+
+def evaluate_model(true_labels, predicted_labels, predicted_probs, label_names):
     """Evaluate the CNN model.
 
     This function evaluates the CNN model and produces classification report and confusion matrix
@@ -82,25 +77,28 @@ def evaluate_model(true_labels, predict_probs, label_names):
     """
     try:
 
+        true_labels_1D = true_labels[:, 0]
+        predicted_labels_1D = predicted_labels[:, 0]
+        predicted_probs_1D = predicted_probs[:, 0]
         # Calculates accuracry, precision, recall and f1 scores.
-        print(f"Accuracy: {accuracy_score(true_labels, predict_probs.round())}")
-        print(f"Precision: {precision_score(true_labels, predict_probs.round(), average='weighted')}")
-        print(f"Recall: {recall_score(true_labels, predict_probs.round(), average='weighted')}")
-        print(f"F1 Score: {f1_score(true_labels, predict_probs.round(), average='weighted')}")
+        print(f"Accuracy: {accuracy_score(true_labels_1D, predicted_labels_1D)}")
+        print(f"Precision: {precision_score(true_labels_1D, predicted_labels_1D)}")
+        print(f"Recall: {recall_score(true_labels_1D, predicted_labels_1D)}")
+        print(f"F1 Score: {f1_score(true_labels_1D, predicted_labels_1D)}")
 
         # Performs classification report
         print("Classification report : ")
-        print(classification_report(true_labels, predict_probs.round(), target_names=label_names))
+        print(classification_report(true_labels_1D, predicted_labels_1D, target_names=label_names))
 
         # Generates confusion matrix
-        matrix = confusion_matrix(true_labels, predict_probs.round())
+        matrix = confusion_matrix(true_labels, predicted_labels_1D)
         ConfusionMatrixDisplay(matrix, display_labels=label_names).plot(cmap=plt.cm.Blues)
         if label_names is not None:
             tick_marks = np.arange(len(label_names))
             plt.title("CNN Confusion Matrix Display")
             plt.xticks(tick_marks, label_names)
             plt.yticks(tick_marks, label_names)
-            plt.savefig('A/figures/Confusion_Matrix_test1.png')
+            plt.savefig('A/figures/Confusion_Matrix_CNN.png')
 
     except Exception as e:
         print(f"Evaluating the model has failed. Error: {e}")
@@ -136,7 +134,7 @@ def class_imbalance_handling(train_dataset):
     
     except Exception as e:
         print(f"Class imbalance handling has failed. Error: {e}")
-        
+
 
 def CNN_model_training(train_dataset, validation_dataset, test_dataset):
     """CNN model training
@@ -159,39 +157,24 @@ def CNN_model_training(train_dataset, validation_dataset, test_dataset):
         train_labels = np.array(train_dataset.labels)
         val_imgs = np.array(validation_dataset.imgs)
         val_labels = np.array(validation_dataset.labels)
-
-        data_augmentation = Sequential([
-            RandomFlip(mode="horizontal_and_vertical"),
-            RandomRotation(0.2),
-            RandomZoom(0.3),
-            ]) 
  
         # CNN model
         model = Sequential()
         model.add(Input(shape=(28,28,1)))
-        model.add(data_augmentation)
         model.add(Conv2D(32, (3,3), padding='same', activation="relu"))
-        model.add(BatchNormalization())
         model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), padding='same'))
-        #model.add(Dropout(0.25))
-        model.add(Conv2D(32, (3,3), padding='same', activation="relu"))
-        model.add(Conv2D(64, (3,3), kernel_initializer='he_uniform', activation="relu"))
-        model.add(Conv2D(128, (3,3), padding='same', activation="relu"))
-        model.add(MaxPooling2D(pool_size=(2,2), strides=(1,1), padding='same'))
-
         model.add(Flatten())
         model.add(Dense(512, activation="relu"))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation="sigmoid"))
 
-        model.build(input_shape=(None, 28, 28, 1))
 
         # Output the model summary
         print(model.summary())
 
         # Plot the CNN model
         plot_model(model, 
-                to_file='A/figures/CNN_Model_taskA_final_add.png', 
+                to_file='A/figures/CNN_Model_taskA_add.png', 
                 show_shapes=True,
                     show_dtype=False,
                     show_layer_names=False,
@@ -203,29 +186,27 @@ def CNN_model_training(train_dataset, validation_dataset, test_dataset):
 
         # Compile the CNN model
         model.compile(loss='binary_crossentropy',
-                optimizer=Adam(learning_rate=0.0001),
+                optimizer=Adam(learning_rate=0.0005),
                 metrics=['accuracy'])
         
         # Handle the class imbalance.
         weights = class_imbalance_handling(train_dataset)
 
-        augmented_data = data_augmentation(train_imgs)
-        print(f"Augmented data size is {str(augmented_data.shape)}")
         # Fit the CNN model
-        history = model.fit(augmented_data, train_labels,
-                epochs=100,
-                callbacks=[tf.keras.callbacks.EarlyStopping(patience=10)],
-                validation_data=(val_imgs, val_labels),
-                batch_size=32,
-                shuffle=True,
-                class_weight=weights)
+        history = model.fit(train_imgs, train_labels, 
+                            batch_size=32,
+                            epochs=30,
+                            callbacks=[tf.keras.callbacks.EarlyStopping(patience=10)],
+                            validation_data=(val_imgs, val_labels),
+                            shuffle=True,
+                            class_weight=weights)
         
-        utils.save_model("A",model, "CNN_model_taskA_final_add")
+        utils.save_model("A",model, "CNN_model_taskA_add")
 
         # Evaluate the model
-        test_dataset_prob = model.predict(test_dataset.imgs, verbose=0)
-        #test_predict_labels = np.argmax(test_dataset_prob, axis=-1)
-        evaluate_model(test_dataset.labels, test_dataset_prob, class_labels)
+        test_predict_prob = model.predict(test_dataset.imgs, verbose=0)
+        test_predict_labels = np.where(test_predict_prob > 0.5, 1, 0)
+        evaluate_model(test_dataset.labels, test_predict_labels, test_predict_prob, class_labels)
         utils.plot_accuray_loss("A",history)
 
     except Exception as e:
@@ -252,10 +233,9 @@ def CNN_model_testing(test_dataset):
         model = utils.load_model("A", "CNN_model_taskA_final")
 
         # Evaluate the model
-        test_dataset_prob = model.predict(test_dataset.imgs, verbose=0)
-
-        #test_predict_labels = np.argmax(test_dataset_prob, axis=-1)
-        evaluate_model(test_dataset.labels, test_dataset_prob, class_labels)
+        test_predict_prob = model.predict(test_dataset.imgs, verbose=0)
+        test_predict_labels = np.where(test_predict_prob > 0.5, 1, 0)
+        evaluate_model(test_dataset.labels, test_predict_labels, test_predict_prob, class_labels)
         
 
     except Exception as e:
